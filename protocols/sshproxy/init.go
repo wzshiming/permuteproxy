e190a68c0a6d0c02fc32fd1dd86dc31eae1e1b28
Shiming Zhang <wzshiming@foxmail.com>
2023-04-09 01:25:04 +0800

    Add ssh command proxy

--- protocols/sshproxy/init.go
+++ protocols/sshproxy/init.go
@@ -6,28 +6,86 @@ import (
 )
 
 func init() {
-	define := permuteproxy.Define{
-		SchemeInfo: protocols.SchemeInfo{
-			Kind:          protocols.KindProxy,
-			Base:          protocols.KindStream,
-			UsernameField: KeyUsername,
-			PasswordField: KeyPassword,
-			MetaFields: []string{
-				"identity_data",
-				"identity_file",
-				"authorized_data",
-				"authorized_file",
-				"hostkey_data",
-				"hostkey_file",
+	metas := map[string]permuteproxy.Define{
+		"ssh": {
+			SchemeInfo: protocols.SchemeInfo{
+				Kind:          protocols.KindProxy,
+				Base:          protocols.KindStream,
+				UsernameField: KeyUsername,
+				PasswordField: KeyPassword,
+				MetaFields: []string{
+					"identity_data",
+					"identity_file",
+					"authorized_data",
+					"authorized_file",
+					"hostkey_data",
+					"hostkey_file",
+				},
+				DialSupport: []string{
+					"tcp",
+				},
 			},
-			DialSupport: []string{
-				"tcp",
+			Handler: permuteproxy.Handler{
+				DialerWrapper: permuteproxy.DialerWrapperFunc(NewSSHProxyDialer),
+				NewRunner:     permuteproxy.NewRunnerFunc(NewSSHProxyRunner),
 			},
 		},
-		Handler: permuteproxy.Handler{
-			DialerWrapper: permuteproxy.DialerWrapperFunc(NewSSHProxyDialer),
-			NewRunner:     permuteproxy.NewRunnerFunc(NewSSHProxyRunner),
+		"ssh-cmd": {
+			SchemeInfo: protocols.SchemeInfo{
+				Kind:          protocols.KindProxy,
+				Base:          protocols.KindStream,
+				UsernameField: KeyUsername,
+				PasswordField: KeyPassword,
+				MetaFields: []string{
+					"identity_data",
+					"identity_file",
+				},
+				DialSupport: []string{
+					"tcp",
+				},
+			},
+			Handler: permuteproxy.Handler{
+				DialerWrapper: permuteproxy.DialerWrapperFunc(NewSSHProxyCommandDialer),
+			},
+		},
+		"ssh-cmd-nc": {
+			SchemeInfo: protocols.SchemeInfo{
+				Kind:          protocols.KindProxy,
+				Base:          protocols.KindStream,
+				UsernameField: KeyUsername,
+				PasswordField: KeyPassword,
+				MetaFields: []string{
+					"identity_data",
+					"identity_file",
+				},
+				DialSupport: []string{
+					"tcp",
+				},
+			},
+			Handler: permuteproxy.Handler{
+				DialerWrapper: permuteproxy.DialerWrapperFunc(NewSSHProxyCommandNetCatDialer),
+			},
+		},
+		"ssh-cmd-nc-unix": {
+			SchemeInfo: protocols.SchemeInfo{
+				Kind:          protocols.KindProxy,
+				Base:          protocols.KindStream,
+				UsernameField: KeyUsername,
+				PasswordField: KeyPassword,
+				MetaFields: []string{
+					"identity_data",
+					"identity_file",
+				},
+				DialSupport: []string{
+					"tcp",
+				},
+			},
+			Handler: permuteproxy.Handler{
+				DialerWrapper: permuteproxy.DialerWrapperFunc(NewSSHProxyCommandNetCatUnixDialer),
+			},
 		},
 	}
-	permuteproxy.Registry("ssh", define)
+	for scheme, meta := range metas {
+		permuteproxy.Registry(scheme, meta)
+	}
 }
